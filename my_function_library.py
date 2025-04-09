96b54d69bb994c4278373c54df2d0e6f3877932a
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2025-04-09 20:19:50 +0200

    Fonction pour détecter les OUTLIERS

---
+++ my_function_library.py
@@ -0,0 +1,11 @@
+"""---------------------- Nettoyage des données -------------------"""
+# Fonction pour détecter les OUTLIERS
+def iqr_outlier_detection(df, feature, threshold=1.5):
+    q1 = df[feature].quantile(0.25)
+    q3 = df[feature].quantile(0.75)
+    iqr = q3 - q1
+
+    lower_bound = q1 - threshold * iqr
+    upper_bound = q3 + threshold * iqr
+
+    return df.query(f"{feature} < {lower_bound} | {feature} > {upper_bound}")
