5d9c569a352a849429b2df244f9ac108c2c33096
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2025-04-18 14:42:54 +0200

    corr and heatmap function

--- my_function_library.py
+++ my_function_library.py
@@ -12,3 +12,21 @@ def iqr_outlier_detection(df, feature, threshold=1.5):
     df_cleaned = df.query(f"{feature} >= {lower_bound} | {feature} <= {upper_bound}")
 
     return outliers, df_cleaned
+
+
+# Heat map
+def get_corr_matrix_and_heatmap(df):
+
+    # Calculate correlation
+    corr_matrix = df.corr(numeric_only=True)
+
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
+
+    # Set up the matplotlib figure
+    plt.style.use('seaborn-v0_8-darkgrid')
+    fig, ax = plt.subplots(figsize=(8, 5))
+
+    # Plot the correlation matrix
+    sns.heatmap(corr_matrix, mask=mask, cmap='coolwarm', annot=True, fmt='.1f', linewidths=0.5, square=True, ax=ax)
+    return corr_matrix, fig
